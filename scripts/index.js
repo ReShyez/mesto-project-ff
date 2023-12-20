c2af673abdba4577df7c1edc31f4eebaae5ae96b
Panov Nikita <reshyez@gmail.com>
2023-12-20 18:31:15 +0300

    finally?

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,29 @@
 const listCards = document.querySelector('.places__list');
 // Получили содержимое темплейта
 const templateCard = document.querySelector('#card-template').content;
+//функция удаление карточки
+function deleteCard(el) {
+  el.remove();
+}
 //функция сборки карточки
-function renderCard({name, link}) {
+function renderCard({name, link}, delButton) {
 	const userCard = templateCard.querySelector('.places__item').cloneNode(true);
   //Переменная кнопки
   const trashButton =  userCard.querySelector('.card__delete-button');
   //слушатель кнопки удаления
-  trashButton.addEventListener('click', deleteCard);
+  trashButton.addEventListener('click', () => delButton(userCard));
   //подстановка значений из массива
   userCard.querySelector('.card__image').src =  link;
+  userCard.querySelector('.card__image').alt =  `На этой карточке ${name}`;
   userCard.querySelector('.card__title').textContent = name;
-  listCards.prepend(userCard);
+  return userCard;
 }
-//функция удаление карточки
-function deleteCard() {
-  let delCard = document.querySelector('.card');
-  delCard.remove();
+//функция создания набора карточек на страние
+function addCards () {
+  initialCards.forEach(function (userCard) {
+      const card = renderCard(userCard,deleteCard);
+      listCards.prepend(card);
+  });
 }
 
-initialCards.forEach(renderCard);
+addCards();
